325be1d339a0adb0e6e6ca10f81f48c30d86dc89
Rob Brennan <rob@therobbrennan.com>
2021-06-27 13:41:12 -0700

    Update page title for default page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Web Audio Input Visualizer</title>
+        <title>Web Audio Oscillator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
